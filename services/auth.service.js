291534abe85d9965333c5d8eb9eee6c0b7eea99a
Yoav Natan <yoavnatan.yn@gmail.com>
2025-11-18 18:44:39 +0200

    Fix secret bug

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,6 +1,6 @@
 import Cryptr from 'cryptr'
 import { userService } from './user.service.js'
-const cryptr = new Cryptr(process.env.SECRET1 ?? 'secret-puk-1234')
+const cryptr = new Cryptr(process.env.SECRET1 || 'secret-puk-1234')
 
 export const authService = {
     checkLogin,
@@ -24,6 +24,7 @@ function checkLogin({ username, password }) {
 function getLoginToken(user) {
     const userInfo = { _id: user._id, fullname: user.fullname, isAdmin: user.isAdmin }
     let str = JSON.stringify(userInfo)
+    console.log('encryptedStr')
     const encryptedStr = cryptr.encrypt(str)
     return encryptedStr
 }
